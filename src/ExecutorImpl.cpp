4344ab6d51ea83bb1151a6016b78797ab0b4bbb7
Kohn24 <2519762723@qq.com>
2024-11-26 20:40:21 +0800

    Hanlinwan extract Move()

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -1,7 +1,7 @@
 #include "ExecutorImpl.hpp" 
 namespace adas
 {
-    // 这里重新定义了抽象类的构造函数，并且用自己的构造函数代替
+    // 这里实现了抽象类的构造函数，返回的是一个子类
     Executor* Executor::NewExecutor(const Pose& pose) noexcept
     {
         return new (std::nothrow) ExecutorImpl(pose);
@@ -16,23 +16,7 @@ namespace adas
             switch (cmd)
             {
             case 'M':
-                switch (pose.heading)
-                {
-                case 'N':
-                    pose.y++;
-                    break;
-                case 'S':
-                    pose.y--;
-                    break;
-                case 'E':
-                    pose.x++;
-                    break;
-                case 'W':
-                    pose.x--;
-                    break;
-                default:
-                    break;
-                }
+                Move();
                 break;
             case 'L':
                 switch (pose.heading)
@@ -81,5 +65,17 @@ namespace adas
     Pose ExecutorImpl::Query() const noexcept
     {
         return pose;
+    }
+    void ExecutorImpl::Move() noexcept
+    {
+        if (pose.heading == 'E') {
+            ++pose.x;
+        } else if (pose.heading == 'W') {
+            --pose.x;
+        } else if (pose.heading == 'N') {
+            ++pose.y;
+        } else if (pose.heading == 'S') {
+            --pose.y;
+        }
     }
 }
