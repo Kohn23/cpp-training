4434dfe4ecbc9d245b0a32b8b8b194df5836f384
Kohn24 <2519762723@qq.com>
2024-11-26 22:12:46 +0800

    Hanlinwan TurnRightCommand support FastMode pass: all_18_tests

--- src/ExecutorImpl.hpp
+++ src/ExecutorImpl.hpp
@@ -63,6 +63,10 @@ private:
     public:
         void DoOperate(ExecutorImpl& executor) const noexcept override
         {
+            if(executor.isFast())
+            {
+                executor.Move();
+            }
             executor.TurnRight();
         }
     };
