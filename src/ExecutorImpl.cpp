461eb04f7d50c91915d6cea73588a3070ef2999c
Kohn24 <2519762723@qq.com>
2024-12-15 00:19:31 +0800

    Hanlinwan Atom Actions successfully passed all tests

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -19,7 +19,7 @@ ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept : poseHandler(pose)
 void ExecutorImpl::Execute(const std::string& commands) noexcept
 {
     const auto cmders = cmderFactory->GetCmders(commands);
-    std::for_each(cmders.begin(), cmders.end(),[this](const Cmder& cmder) noexcept { cmder(poseHandler); });
+    std::for_each(cmders.begin(), cmders.end(), [this](const Cmder& cmder) noexcept { cmder(poseHandler).DoOperate(poseHandler); });
 }
 
 Pose ExecutorImpl::Query() const noexcept
