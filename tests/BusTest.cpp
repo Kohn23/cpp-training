9b9b3d1002ebdcade8f3daaf4915eea6833b6273
Kohn24 <2519762723@qq.com>
2024-12-15 19:16:19 +0800

    Hanlinwan Bus functuions complete and passed all tests

--- tests/BusTest.cpp
+++ tests/BusTest.cpp
@@ -16,7 +16,7 @@ TEST(BusTest, should_return_x_plus_1_given_command_is_M_and_facing_is_E)
     executor->Execute("M");
     // then
     const Pose targetHead({1, 0, 'E'});
-    const Pose targetTail({1, 0, 'E'});
+    const Pose targetTail({0, 0, 'E'});
     ASSERT_EQ(targetHead, executor->QueryHead());
     ASSERT_EQ(targetTail, executor->QueryTail());
 }
@@ -28,8 +28,10 @@ TEST(BusTest, should_return_x_minus_1_given_command_is_M_and_facing_is_W)
     // when
     executor->Execute("M");
     // then
-    const Pose targetHead({-2, 0, 'W'});
+    const Pose targetHead({-1, 0, 'W'});
+    const Pose targetTail({0, 0, 'W'});
     ASSERT_EQ(targetHead, executor->QueryHead());
+    ASSERT_EQ(targetTail, executor->QueryTail());
 }
 
 TEST(BusTest, should_return_y_plus_1_given_command_is_M_and_facing_is_N)
@@ -39,8 +41,10 @@ TEST(BusTest, should_return_y_plus_1_given_command_is_M_and_facing_is_N)
     // when
     executor->Execute("M");
     // then
-    const Pose targetHead({0, 2, 'N'});
+    const Pose targetHead({0, 1, 'N'});
+    const Pose targetTail({0, 0, 'N'});
     ASSERT_EQ(targetHead, executor->QueryHead());
+    ASSERT_EQ(targetTail, executor->QueryTail());
 }
 
 TEST(BusTest, should_return_y_minus_1_given_command_is_M_and_facing_is_S)
@@ -50,8 +54,10 @@ TEST(BusTest, should_return_y_minus_1_given_command_is_M_and_facing_is_S)
     // when
     executor->Execute("M");
     // then
-    const Pose targetHead({0, -2, 'S'});
+    const Pose targetHead({0, -1, 'S'});
+    const Pose targetTail({0, 0, 'S'});
     ASSERT_EQ(targetHead, executor->QueryHead());
+    ASSERT_EQ(targetTail, executor->QueryTail());
 }
 
 TEST(BusTest, should_return_facing_N_and_x_plus_1_given_command_is_L_and_facing_is_E)
@@ -61,8 +67,10 @@ TEST(BusTest, should_return_facing_N_and_x_plus_1_given_command_is_L_and_facing_
     // when
     executor->Execute("L");
     // then
-    const Pose targetHead({0, 1, 'N'});
+    const Pose targetHead({1, 0, 'N'});
+    const Pose targetTail({1, -1, 'N'});
     ASSERT_EQ(targetHead, executor->QueryHead());
+    ASSERT_EQ(targetTail, executor->QueryTail());
 }
 
 TEST(BusTest, should_return_facing_S_and_x_minus_1_given_command_is_L_and_facing_is_W)
@@ -72,29 +80,34 @@ TEST(BusTest, should_return_facing_S_and_x_minus_1_given_command_is_L_and_facing
     // when
     executor->Execute("L");
     // then
-    const Pose targetHead({0, -1, 'S'});
+    const Pose targetHead({-1, 0, 'S'});
+    const Pose targetTail({-1, 1, 'S'});
     ASSERT_EQ(targetHead, executor->QueryHead());
+    ASSERT_EQ(targetTail, executor->QueryTail());
 }
 
-TEST(BusTest, should_return_facing_W_and_y_minus_1_given_command_is_L_and_facing_is_N)
+TEST(BusTest, should_return_facing_W_and_y_plus_1_given_command_is_L_and_facing_is_N)
 {
     // given
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}, 'B'));
     // when
     executor->Execute("L");
     // then
-    const Pose targetHead({-1, 0, 'W'});
+    const Pose targetHead({0, 1, 'W'});
+    const Pose targetTail({1, 1, 'W'});
     ASSERT_EQ(targetHead, executor->QueryHead());
+    ASSERT_EQ(targetTail, executor->QueryTail());
 }
 
-TEST(BusTest, should_return_facing_E_and_y_plus_1_given_command_is_L_and_facing_is_S)
+TEST(BusTest, should_return_facing_E_and_y_minus_1_given_command_is_L_and_facing_is_S)
 {
     // given
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'}, 'B'));
     // when
     executor->Execute("L");
     // then
-    const Pose targetHead({1, 0, 'E'});
+    const Pose targetHead({0, -1, 'E'});
+    const Pose targetTail({-1, -1, 'E'});
     ASSERT_EQ(targetHead, executor->QueryHead());
 }
 
@@ -105,8 +118,10 @@ TEST(BusTest, should_return_facing_E_and_y_plus_1_given_command_is_R_and_facing_
     // when
     executor->Execute("R");
     // then
-    const Pose targetHead({1, 0, 'E'});
+    const Pose targetHead({0, 1, 'E'});
+    const Pose targetTail({-1, 1, 'E'});
     ASSERT_EQ(targetHead, executor->QueryHead());
+    ASSERT_EQ(targetTail, executor->QueryTail());
 }
 
 TEST(BusTest, should_return_facing_W_and_y_minus_1_given_command_is_R_and_facing_is_S)
@@ -116,41 +131,49 @@ TEST(BusTest, should_return_facing_W_and_y_minus_1_given_command_is_R_and_facing
     // when
     executor->Execute("R");
     // then
-    const Pose targetHead({-1, 0, 'W'});
+    const Pose targetHead({0, -1, 'W'});
+    const Pose targetTail({1,-1, 'W'});
     ASSERT_EQ(targetHead, executor->QueryHead());
+    ASSERT_EQ(targetTail, executor->QueryTail());
 }
 
-TEST(BusTest, should_return_facing_S_and_x_minus_1_given_command_is_R_and_facing_is_E)
+TEST(BusTest, should_return_facing_S_and_x_plus_1_given_command_is_R_and_facing_is_E)
 {
     // given
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}, 'B'));
     // when
     executor->Execute("R");
     // then
-    const Pose targetHead({0, -1, 'S'});
+    const Pose targetHead({1, 0, 'S'});
+    const Pose targetTail({1, 1, 'S'});
     ASSERT_EQ(targetHead, executor->QueryHead());
+    ASSERT_EQ(targetTail, executor->QueryTail());
 }
 
-TEST(BusTest, should_return_facing_N_and_x_plus_1_given_command_is_R_and_facing_is_W)
+TEST(BusTest, should_return_facing_N_and_x_minus_1_given_command_is_R_and_facing_is_W)
 {
     // given
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'}, 'B'));
     // when
     executor->Execute("R");
     // then
-    const Pose targetHead({0, 1, 'N'});
+    const Pose targetHead({-1, 0, 'N'});
+    const Pose targetTail({-1, -1, 'N'});
     ASSERT_EQ(targetHead, executor->QueryHead());
+    ASSERT_EQ(targetTail, executor->QueryTail());
 }
 
-TEST(BusFastTest, should_return_x_plus_4_given_status_is_fast_command_is_M_and_facing_is_E)
+TEST(BusFastTest, should_return_x_plus_2_given_status_is_fast_command_is_M_and_facing_is_E)
 {
     // given
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}, 'B'));
     // when
     executor->Execute("FM");
     // then
-    const Pose targetHead{4, 0, 'E'};
+    const Pose targetHead{2, 0, 'E'};
+    const Pose targetTail{1, 0, 'E'};
     ASSERT_EQ(targetHead, executor->QueryHead());
+    ASSERT_EQ(targetTail, executor->QueryTail());
 }
 
 TEST(BusFastTest, should_return_N_and_x_plus_2_given_status_is_fast_command_is_L_and_facing_is_E)
@@ -160,19 +183,23 @@ TEST(BusFastTest, should_return_N_and_x_plus_2_given_status_is_fast_command_is_L
     // when
     executor->Execute("FL");
     // then
-    const Pose targetHead{1, 1, 'N'};
+    const Pose targetHead{2, 0, 'N'};
+    const Pose targetTail{2, -1, 'N'};
     ASSERT_EQ(targetHead, executor->QueryHead());
+    ASSERT_EQ(targetTail, executor->QueryTail());
 }
 
 TEST(BusFastTest, should_return_S_and_x_plus_2_given_status_is_fast_given_command_is_R_and_facing_is_E)
 {
     // given
-    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}, 'S'));
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}, 'B'));
     // when
     executor->Execute("FR");
     // then
-    const Pose targetHead{1, -1, 'S'};
+    const Pose targetHead{2, 0, 'S'};
+    const Pose targetTail{2, 1, 'S'};
     ASSERT_EQ(targetHead, executor->QueryHead());
+    ASSERT_EQ(targetTail, executor->QueryTail());
 }
 
 TEST(BusFastTest, should_return_y_plus_1_given_command_is_FFM_and_facing_is_N)
@@ -182,7 +209,7 @@ TEST(BusFastTest, should_return_y_plus_1_given_command_is_FFM_and_facing_is_N)
     // when
     executor->Execute("FFM");
     // then
-    const Pose targetHead{0, 2, 'N'};
+    const Pose targetHead{0, 1, 'N'};
     ASSERT_EQ(targetHead, executor->QueryHead());
 }
 
@@ -193,8 +220,10 @@ TEST(BusBackTest, should_return_y_minus_1_given_status_is_backward_command_is_M_
     // when
     executor->Execute("BM");
     // then
-    const Pose targetHead{0, -2, 'N'};
+    const Pose targetHead{0, -1, 'N'};
+    const Pose targetTail{0, -2, 'N'};
     ASSERT_EQ(targetHead, executor->QueryHead());
+    ASSERT_EQ(targetTail, executor->QueryTail());
 }
 
 TEST(BusBackTest, should_return_S_and_x_minus_1_given_status_is_backward_command_is_L_and_facing_is_E)
@@ -204,8 +233,10 @@ TEST(BusBackTest, should_return_S_and_x_minus_1_given_status_is_backward_command
     // when
     executor->Execute("BL");
     // then
-    const Pose targetHead{0, 1, 'S'};
+    const Pose targetHead{-1, 0, 'S'};
+    const Pose targetTail{-1, 1, 'S'};
     ASSERT_EQ(targetHead, executor->QueryHead());
+    ASSERT_EQ(targetTail, executor->QueryTail());
 }
 
 TEST(BusBackTest, should_return_N_and_x_minus_1_given_status_is_backward_command_is_R_and_facing_is_E)
@@ -215,8 +246,10 @@ TEST(BusBackTest, should_return_N_and_x_minus_1_given_status_is_backward_command
     // when
     executor->Execute("BR");
     // then
-    const Pose targetHead{0, -1, 'N'};
+    const Pose targetHead{-1, 0, 'N'};
+    const Pose targetTail{-1, -1, 'N'};
     ASSERT_EQ(targetHead, executor->QueryHead());
+    ASSERT_EQ(targetTail, executor->QueryTail());
 }
 
 TEST(BusBackTest, should_return_x_minus_2_given_status_is_fast_and_backward_command_is_M_and_facing_is_E)
@@ -226,8 +259,10 @@ TEST(BusBackTest, should_return_x_minus_2_given_status_is_fast_and_backward_comm
     // when
     executor->Execute("BFM");
     // then
-    const Pose targetHead{-4, 0, 'E'};
+    const Pose targetHead{-2, 0, 'E'};
+    const Pose targetTail{-3, 0, 'E'};
     ASSERT_EQ(targetHead, executor->QueryHead());
+    ASSERT_EQ(targetTail, executor->QueryTail());
 }
 
 TEST(BusBackTest, should_return_S_and_x_minus_2_given_status_is_fast_and_backward_command_is_L_and_facing_is_E)
@@ -237,8 +272,10 @@ TEST(BusBackTest, should_return_S_and_x_minus_2_given_status_is_fast_and_backwar
     // when
     executor->Execute("BFL");
     // then
-    const Pose targetHead{-1, 1, 'S'};
+    const Pose targetHead{-2, 0, 'S'};
+    const Pose targetTail{-2, 1, 'S'};
     ASSERT_EQ(targetHead, executor->QueryHead());
+    ASSERT_EQ(targetTail, executor->QueryTail());
 }
 
 TEST(BusBackTest, should_return_N_and_x_minus_2_given_status_is_fast_and_backward_command_is_R_and_facing_is_E)
@@ -248,8 +285,10 @@ TEST(BusBackTest, should_return_N_and_x_minus_2_given_status_is_fast_and_backwar
     // when
     executor->Execute("BFR");
     // then
-    const Pose targetHead{-1, -1, 'N'};
+    const Pose targetHead{-2, 0, 'N'};
+    const Pose targetTail{-2, -1, 'N'};
     ASSERT_EQ(targetHead, executor->QueryHead());
+    ASSERT_EQ(targetTail, executor->QueryTail());
 }
 
 TEST(BusBackTest, should_return_y_plus_1_given_command_is_BBM_and_facing_is_N)
@@ -259,8 +298,10 @@ TEST(BusBackTest, should_return_y_plus_1_given_command_is_BBM_and_facing_is_N)
     // when
     executor->Execute("BBM");
     // then
-    const Pose targetHead{0, 2, 'N'};
+    const Pose targetHead{0, 1, 'N'};
+    const Pose targetTail{0, 0, 'N'};
     ASSERT_EQ(targetHead, executor->QueryHead());
+    ASSERT_EQ(targetTail, executor->QueryTail());
 }
 
 }  // namespace adas
